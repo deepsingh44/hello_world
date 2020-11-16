285e92208615044f84470c0edade5d21b127fe21
DEEP <deepsingh44@gmail.com>
2020-11-16 20:38:16 +0530

    Login query update in database.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hello_world/home_page.dart';
 import 'package:hello_world/login.dart';
 import 'package:hello_world/register.dart';
-
+//test login ui here
 void main() {
   runApp(LoginPage());
 }
